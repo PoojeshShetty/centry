5bdec96c031780f4eac47b4ceecef4a7b0b73259
Poojesh Shetty <shettypoojesh@live.com>
2026-06-15 06:15:19 +0530

    feat(backend): integrate sdk with backend to trace logs to system

--- packages/backend/src/models/index.ts
+++ packages/backend/src/models/index.ts
@@ -2,3 +2,8 @@ export { Account } from './account.js';
 export { Project } from './project.js';
 export { ProjectKey } from './projectKey.js';
 export { Log } from './log.js';
+
+import { Project } from './project.js';
+import { ProjectKey } from './projectKey.js';
+
+ProjectKey.belongsTo(Project, { foreignKey: 'project_id', as: 'project' });
